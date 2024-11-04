e7f496f0a480c0733598b8950e30da7dcccb03fd
Jesus Torres <j.torres3.dev@gmail.com>
2024-11-04 02:01:27 -0600

    sound wav project started

---
+++ sound_wave_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.io import wavfile
+
+sample_rate, data = wavfile.read("/mic-signal-tone.wav")
+print("Sample rate: ", sample_rate)
+print("Data shape:", data.shape)
+
+# calculate time values for plotting
+time = np.linspace(0, len(data)/sample_rate, num=len(data))
+
+# 
+log_data = 10 * np.log10(np.abs(data) + 1)
+
+# plt.figure(figsize=(12, 6))
+plt.plot( log_data)
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [dB]")
+plt.title("Sound wave analysis")
+plt.show()
